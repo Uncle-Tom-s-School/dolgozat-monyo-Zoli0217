b1c350c89e95f2ec4a5a9def877ea6552a14af9e
Zoli0217 <zoltanbodi61@gmail.com>
2025-10-21 12:49:40 +0200

    dolgozat befejezve

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import { useEffect, useState } from "react"
 import BeerCard from "./components/BeerCard"
+import type { BeerType } from "./components/BeerCard"
 
 const App = () => {
+
+  const [beer, setBeer] = useState<BeerType[]>([])
+
+  useEffect(() => {
+    fetch("data.json")
+    .then(res => res.json())
+    .then(data => setBeer(data))
+  }, [])
+
   return (
     <div className="beerCardWrapper">
-      <BeerCard />
+      {beer.map(item => <BeerCard {...item}/> )}
     </div>
   )
 }
--- src/components/BeerCard.tsx
+++ src/components/BeerCard.tsx
@@ -1,6 +1,21 @@
-const BeerCard = () => {
+export type BeerType = {
+  image: string,
+  name: string,
+  abv: string,
+  volume: string,
+  price: string,
+  available: boolean,
+
+}
+
+const BeerCard = (props: BeerType) => {
   return (
-    <div className='beerCard'>Beer Card</div>
+    <div className='beerCard'>
+      <img src={props.image} alt="" />
+      <strong>{props.name} {props.abv} {props.volume}</strong>
+      <p>{props.price} Ft</p>
+      <strong className={props.available? "available" : "notAvailable"}>{props.available ? "Raktáron" : "Nem elérhető"}</strong>
+    </div>
   )
 }
 
